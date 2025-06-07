25a09df95f38c0ab539b9d15a13642c8e1186a70
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 19:32:12 +0200

    refac: multiples of 15 -> FizzBuzz

--- src/main/java/com/github/adrien/cuisse/FizzBuzz.java
+++ src/main/java/com/github/adrien/cuisse/FizzBuzz.java
@@ -4,11 +4,7 @@ public final class FizzBuzz
 {
 	public String generate(int number)
 	{
-		if (number == 15)
-			return "FizzBuzz";
-		if (number == 30)
-			return "FizzBuzz";
-		if (number == 45)
+		if (number % 15 == 0)
 			return "FizzBuzz";
 		if (number % 5 == 0)
 			return "Buzz";
