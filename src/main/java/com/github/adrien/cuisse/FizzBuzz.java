6a51bd7ef0af1152496504586fd3f71d0f480a36
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 19:02:10 +0200

    refac: any number

--- src/main/java/com/github/adrien/cuisse/FizzBuzz.java
+++ src/main/java/com/github/adrien/cuisse/FizzBuzz.java
@@ -4,10 +4,6 @@ public final class FizzBuzz
 {
 	public String generate(int number)
 	{
-		if (number == 1)
-			return "1";
-		if (number == 2)
-			return "2";
-		return "4";
+		return String.valueOf(number);
 	}
 }
