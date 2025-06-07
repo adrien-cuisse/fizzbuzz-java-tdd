4da6736146b2bbc205432ee1acb2afd7ec6a9324
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 19:18:52 +0200

    passing test: 5 -> Buzz

--- src/main/java/com/github/adrien/cuisse/FizzBuzz.java
+++ src/main/java/com/github/adrien/cuisse/FizzBuzz.java
@@ -4,6 +4,8 @@ public final class FizzBuzz
 {
 	public String generate(int number)
 	{
+		if (number == 5)
+			return "Buzz";
 		if (number % 3 == 0)
 			return "Fizz";
 		return String.valueOf(number);
