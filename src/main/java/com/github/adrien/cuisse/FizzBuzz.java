8e16c3bf42b6cc6159fc74ddeace4ea76f6f5bc6
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 19:33:03 +0200

    refac: reordering cases

--- src/main/java/com/github/adrien/cuisse/FizzBuzz.java
+++ src/main/java/com/github/adrien/cuisse/FizzBuzz.java
@@ -6,10 +6,10 @@ public final class FizzBuzz
 	{
 		if (number % 15 == 0)
 			return "FizzBuzz";
-		if (number % 5 == 0)
-			return "Buzz";
 		if (number % 3 == 0)
 			return "Fizz";
+		if (number % 5 == 0)
+			return "Buzz";
 		return String.valueOf(number);
 	}
 }
