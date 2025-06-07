0fbc00cab8581b7daab12213b692b48644b7b7b2
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 18:26:49 +0200

    setup check

---
+++ src/test/java/com/github/adrien/cuisse/FizzBuzzTests.java
@@ -0,0 +1,15 @@
+package com.github.adrien.cuisse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+final class FizzBuzzTests
+{
+	@Test
+	void setupWorks()
+	{
+		assertThat(true, is(true));
+	}
+}
