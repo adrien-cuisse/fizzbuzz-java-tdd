b0d285f791d3c5cc36d0acd6cc46a0e522c5e0b5
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-06-07 19:28:37 +0200

    passing test: 30 -> FizzBuzz

--- src/main/java/com/github/adrien/cuisse/FizzBuzz.java
+++ src/main/java/com/github/adrien/cuisse/FizzBuzz.java
@@ -6,6 +6,8 @@ public final class FizzBuzz
 	{
 		if (number == 15)
 			return "FizzBuzz";
+		if (number == 30)
+			return "FizzBuzz";
 		if (number % 5 == 0)
 			return "Buzz";
 		if (number % 3 == 0)
